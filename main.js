d24d158435cb8a92b2d5aec518cf341bb0c8ef0e
Pg-Mighty <85114806+Pg-Mighty@users.noreply.github.com>
2025-08-08 14:17:49 +0530

    Commit 2

--- main.js
+++ main.js
@@ -10,25 +10,27 @@ const url = `https://graph.facebook.com/v23.0/${appId}/media`;
 let container_id;
 
 const header = {
-    "Authorization": `Bearer ${accessToken}`,
-    "Content-Type": "multipart/form-data"
+    "Authorization": `Bearer ${accessToken}`
 }
 
 const body ={
     "media_type": "REELS",
-    "video_url": "https://drive.usercontent.google.com/u/0/uc?id=1ApR_d2vPY1W9u2oEGe24Dk9yGkiDfCj0&export=download"
+    "video_url": "https://statxtract.com/reel.mp4"
 }
 
 axios.post(url, body, { headers: header }).then((res)=>{
     container_id = res.data.id;
-}).then(uploadVideo).catch(err=>console.log(err.data));
+    console.log(container_id);
+}).catch(err=>console.log(err.data));
 
+/*
 
 async function uploadVideo(){
 
     const upload_id = await axios.post(`https://rupload.facebook.com/v23.0/${appId}/media_publish`)
 
 }
+*/
 
 
 
