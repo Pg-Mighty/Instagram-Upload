ad73b69656fff95618461d0f03147d2b996ca253
Pg-Mighty <85114806+Pg-Mighty@users.noreply.github.com>
2025-08-12 19:05:57 +0530

    Commit 2

--- promptGen.js
+++ promptGen.js
@@ -7,7 +7,7 @@ const ai = new GoogleGenAI({
 async function main() {
     const response = await ai.models.generateContent({
         model: "gemini-2.5-flash",
-        contents: "Explain how AI works in a few words",
+        contents: "Tell me how AI works in a few words",
     });
     console.log(response.text);
 }
