7c5f16a77b541437950a6670c22bbf2fe6896d86
Pg-Mighty <85114806+Pg-Mighty@users.noreply.github.com>
2025-08-14 20:08:11 +0530

    Commit 2

--- awsupload.js
+++ awsupload.js
@@ -1,12 +1,34 @@
 import {S3Client , PutObjectCommand} from "@aws-sdk/client-s3"
 import env from "dotenv"
+import fs from "fs"
 
+//const file = fs.readFileSync("history.txt").toString();
+const buffer = Buffer.from(file, "base64");
 env.config()
 
-const client = new S3Client({
-    region: "ap-south-1",
-    credentials: {
-        accessKeyId: "sasas",
-        secretAccessKey: "sase2",
-    },
-    })
+async function putObject() {
+    const params = {
+        Bucket: process.env.BUCKET_NAME,
+        Key: "Reel.mp4",
+        Body: buffer,
+        ContentType: "video/mp4"
+    }
+
+    const client = new S3Client({
+        region: "ap-south-1",
+        credentials: {
+            accessKeyId: process.env.aws_access_key,
+            secretAccessKey: process.env.aws_secret,
+        }
+    });
+
+
+    const putConfig = new PutObjectCommand(params)
+
+
+
+    await client.send(putConfig);
+
+}
+
+putObject();
--- main.js
+++ main.js
@@ -8,31 +8,44 @@ const appId = process.env.app_id;
 const url = `https://graph.facebook.com/v23.0/${appId}/media`;
 let container_id;
 
+
+
 const header = {
     "Authorization": `Bearer ${accessToken}`
 }
 
 const body ={
     "media_type": "REELS",
-    "video_url": "https://statxtract.com/reel.mp4"
+    "video_url": "https://statxtract.com/Reel.mp4",
+    "title": "Mars Cutting #asmr #ai #Musical #Video #veo3",
+
 }
 
 axios.post(url, body, { headers: header }).then((res)=>{
     container_id = res.data.id;
-}).then(uploadVideo).catch(err=>console.log(err.data));
+}).then(uploadVideo).catch((err)=>{})
 
 
 
-async function uploadVideo(){
+    async function uploadVideo(){
 
-    const response = await axios.post(`https://graph.facebook.com/v23.0/${appId}/media_publish` , {"creation_id": container_id},
-         {headers:{
-         "Content-Type": "application/json",
-         "Authorization": `Bearer ${accessToken}`
-             }});
+       await new Promise(resolve => setTimeout(resolve, 30000));
+        try {
+            const response = await axios.post(`https://graph.facebook.com/v23.0/${appId}/media_publish`, {"creation_id": container_id},
+                {
+                    headers: {
+                        "Content-Type": "application/json",
+                        "Authorization": `Bearer ${accessToken}`
+                    }
+                })
+            console.log(response.status);
+        }catch(err){
+            console.log(err);
+        }
 
-        console.log(response.data);
-}
+
+
+    }
 
 
 
