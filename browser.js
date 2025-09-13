b58c0f6a35043b94d03c51410aa9f979200f35a4
Pg-Mighty <85114806+Pg-Mighty@users.noreply.github.com>
2025-09-13 20:24:05 +0530

    Commit 2

--- browser.js
+++ browser.js
@@ -36,7 +36,7 @@ async function run(prompt) {
 
         let videoBase64 = await listen(page);
 
-        const videoBuffer = Buffer.from(videoBase64, "base64");////    THis Line
+        const videoBuffer = Buffer.from(videoBase64, "base64");// Buffer 
 
         upload(videoBuffer);
 
