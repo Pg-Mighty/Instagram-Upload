8ec7c5d0360172b94cb07bfce9d7738dc23cae4c
Pg-Mighty <85114806+Pg-Mighty@users.noreply.github.com>
2025-09-22 19:46:09 +0530

    Commit 2

--- awsupload.js
+++ awsupload.js
@@ -1,20 +1,28 @@
 import {S3Client , PutObjectCommand} from "@aws-sdk/client-s3"
 import env from "dotenv"
 import init from "./main.js";
+import edit from "./editor.js";
 
 env.config();
 
-function upload(videoBuffer) {
-    const buffer = Buffer.from(videoBuffer, "base64");
-    putObject(buffer);
+function upload(videoArray) {
+
+    for (let i=0; i<videoArray.length; i++) {
+
+        const buffer = Buffer.from(videoArray[i], "base64");
+        putObject(buffer ,i+1 )
+    }
+
+    edit();
+
 }
 
 
 
-async function putObject(buffer) {
+async function putObject(buffer , index) {
     const params = {
         Bucket: process.env.BUCKET_NAME,
-        Key: "Reel.mp4",
+        Key: `${index}.mp4`,
         Body: buffer,
         ContentType: "video/mp4"
     }
@@ -27,10 +35,11 @@ async function putObject(buffer) {
         }
     });
 
-
     const putConfig = new PutObjectCommand(params)
     await client.send(putConfig);
-    init();
+
+
+    //init();
 
 }
 
