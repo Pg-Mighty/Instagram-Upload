20dd1c423a943537ffcbf38c10efe28c2bba7eaa
Pg-Mighty <85114806+Pg-Mighty@users.noreply.github.com>
2025-09-25 23:50:27 +0530

    Commit 2

--- editor.js
+++ editor.js
@@ -1,6 +1,10 @@
 import ffmpeg from 'fluent-ffmpeg';
 import ffmpegInstaller from '@ffmpeg-installer/ffmpeg';
 import ffprobeInstaller from '@ffprobe-installer/ffprobe';
+import fs from "fs";
+import {putObject as upload} from "./awsupload.js";
+import init from "./main.js";
+
 
 ffmpeg.setFfmpegPath(ffmpegInstaller.path);
 ffmpeg.setFfprobePath(ffprobeInstaller.path);
@@ -12,14 +16,28 @@ try {
         .input("https://reelsformyinstgrampage123.s3.ap-south-1.amazonaws.com/1.mp4")
         .input("https://reelsformyinstgrampage123.s3.ap-south-1.amazonaws.com/2.mp4")
         .input("https://reelsformyinstgrampage123.s3.ap-south-1.amazonaws.com/3.mp4")
-        .mergeToFile('output2.mp4')
+        .mergeToFile('Reel.mp4')
         .run();
 
 }catch (e) {
     console.log(e);
 }
+    finalVideo();
 
-console.log(ffmpegInstaller.path);
 }
+
+function finalVideo(){
+
+
+    fs.readFile("Reel.mp4", (err, data) => {
+
+        if(err){
+            console.log(err);
+        }else{
+            upload(data, "Reel").then(init);
+        }
+    })
+}
+
 
 export default edit;
