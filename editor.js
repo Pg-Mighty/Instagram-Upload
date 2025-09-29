6c5f7061574f361452d13af7e57870054ae026d2
Pg-Mighty <85114806+Pg-Mighty@users.noreply.github.com>
2025-09-29 16:30:02 +0530

    Commit 2

--- editor.js
+++ editor.js
@@ -15,7 +15,6 @@ try {
     await ffmpeg()
         .input("https://reelsformyinstgrampage123.s3.ap-south-1.amazonaws.com/3.mp4")
         .input("https://reelsformyinstgrampage123.s3.ap-south-1.amazonaws.com/3.mp4")
-        .input("https://reelsformyinstgrampage123.s3.ap-south-1.amazonaws.com/3.mp4")
         .mergeToFile('Reel.mp4')
         .output('Reel.mp4')
         .run();
