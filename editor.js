07f81298f5e0dd2658f64d3cfb2583667e7de0bb
Pg-Mighty <85114806+Pg-Mighty@users.noreply.github.com>
2025-09-29 16:27:56 +0530

    Commit 2

--- editor.js
+++ editor.js
@@ -9,22 +9,24 @@ import init from "./main.js";
 ffmpeg.setFfmpegPath(ffmpegInstaller.path);
 ffmpeg.setFfprobePath(ffprobeInstaller.path);
 
-function edit() {
+async function edit() {
     
 try {
-    ffmpeg()
+    await ffmpeg()
         .input("https://reelsformyinstgrampage123.s3.ap-south-1.amazonaws.com/1.mp4")
         .input("https://reelsformyinstgrampage123.s3.ap-south-1.amazonaws.com/2.mp4")
         .input("https://reelsformyinstgrampage123.s3.ap-south-1.amazonaws.com/3.mp4")
-        .mergeToFile('Reel.mp4')
+        .mergeToFile('Reel2.mp4')
+        .output('Reel.mp4')
         .run();
 
 }catch (e) {
     console.log(e);
 }
-    finalVideo();
+ //   finalVideo();
 
 }
+edit();
 
 function finalVideo(){
 
