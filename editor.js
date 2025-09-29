74ba9e79937ad6ce0ad4a489ae5424ec0ce53575
Pg-Mighty <85114806+Pg-Mighty@users.noreply.github.com>
2025-09-29 16:34:21 +0530

    Commit 2

--- editor.js
+++ editor.js
@@ -13,19 +13,17 @@ async function edit() {
     
 try {
     await ffmpeg()
+        .input("https://reelsformyinstgrampage123.s3.ap-south-1.amazonaws.com/1.mp4")
+        .input("https://reelsformyinstgrampage123.s3.ap-south-1.amazonaws.com/2.mp4")
         .input("https://reelsformyinstgrampage123.s3.ap-south-1.amazonaws.com/3.mp4")
-        .input("https://reelsformyinstgrampage123.s3.ap-south-1.amazonaws.com/3.mp4")
-        .mergeToFile('Reel.mp4')
-        .output('Reel.mp4')
-        .run();
+        .mergeToFile('Reel.mp4');
 
 }catch (e) {
     console.log(e);
 }
- //   finalVideo();
+  finalVideo();
 
 }
-edit();
 
 function finalVideo(){
 
