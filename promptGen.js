6f50d76892b704ed68637f3a8941a4c0a0cabea7
Pg-Mighty <85114806+Pg-Mighty@users.noreply.github.com>
2026-03-20 23:56:28 +0530

    Commit 2

--- promptGen.js
+++ promptGen.js
@@ -23,7 +23,7 @@ async function main() {
     for(let i=0; i<3 ;i++) {
 
         const res = await chat.sendMessage({
-                message: "Create",
+                message: "Createeee",
             }
         )
         history = [...chat.getHistory(false)];
