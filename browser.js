de298558005b62b0c985b853462676e657ff0979
Pg-Mighty <85114806+Pg-Mighty@users.noreply.github.com>
2026-04-09 22:38:15 +0530

    Commit 2

--- browser.js
+++ browser.js
@@ -113,7 +113,7 @@ async function run(promptArray) {
         await new Promise(r => setTimeout(r, 17000)); // 17 sec wait
         try {
             const base64Video = await listenForStream(page);
-            console.log(base64Video);
+            console.log(base64Video); //focus on this line
             videoArray.push(base64Video);
             console.log(`Successfully generated video ${i + 1}`);
             await new Promise(r=> setTimeout(r, 3000)); //3 sec pause
